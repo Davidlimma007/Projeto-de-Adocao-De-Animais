548634e8a8704508be79f676aef1e7e59a8bb6bd
David Lima <davidlimma05@gmail.com>
2025-10-23 22:16:33 -0300

    Update Repositorio.java

--- src/main/java/Repositorio.java
+++ src/main/java/Repositorio.java
@@ -1,11 +1,13 @@
 public interface Repositorio {
 
-
-    void salvarAdocao(Adocao adocao);
-
     void salvarAdotante(Adotante adotante);
 
     void salvarAnimal(Animal animal);
-
     void atualizarStatusAnimal(Animal animal);
+
+    void salvarAdocao(Adocao adocao);
+
+    Adotante buscarAdotantePorId(int id);
+    Animal buscarAnimalPorId(int id);
+
 }
