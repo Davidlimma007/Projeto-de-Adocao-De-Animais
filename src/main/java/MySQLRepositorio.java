edd68192e35080fba3b8d66d79f78758c87b3277
David Lima <davidlimma05@gmail.com>
2025-10-23 22:23:12 -0300

    Update MySQLRepositorio.java

--- src/main/java/MySQLRepositorio.java
+++ src/main/java/MySQLRepositorio.java
@@ -1,13 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class MySQLRepositorio {
-
-    // criando a lista internas de animais disponiveis e pessoas cadastradas
-    private List<Animal> animais = new ArrayList<>();
-    private List<Adotante> adotantes = new ArrayList<>();
-    private List<Adocao> adocoes = new ArrayList<>();
 
 
 }
