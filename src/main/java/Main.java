ab455eab4a138b7a965fe4ee98fc31673d468293
David Lima <davidlimma05@gmail.com>
2025-10-25 13:30:42 -0300

    atuaização
    primeiros passos na elaboração da classe MysqlRepositorio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        repository.MySQLRepositorio repositorio = new repository.MySQLRepositorio();
+        System.out.println("Iniciando conexão com o banco de dados.....");
+
+        if(repositorio.testarConexao()){
+            System.out.println("Sucesso na conexão");
+        }else{
+            System.out.println("Falha na conexão");
+        }
 
     }
 
--- src/main/java/repository/MySQLRepositorio.java
+++ src/main/java/repository/MySQLRepositorio.java
@@ -1,5 +1,75 @@
 package repository;
 
-public class MySQLRepositorio {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import model.Adocao;
+import model.Adotante;
+import model.Animal;
+
+
+public class MySQLRepositorio implements Repositorio{
+
+    private static final String URL = "jdbc:mysql://localhost:3306/db_adocao";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+    public Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erro: Driver MySQL não encontrado. O JAR está na pasta 'lib' e configurado?");
+            e.printStackTrace();
+            // Lança um erro fatal se o Java não encontrar o 'tradutor'
+            throw new RuntimeException("Driver MySQL não encontrado.", e);
+        }
+
+        // Tenta estabelecer a conexão usando o URL, Usuário e Senha
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public boolean testarConexao(){
+        try(Connection connection = getConnection()){
+            System.out.println("Status da conexão: Conectado com sucesso!");
+            return connection != null;
+        }catch (SQLException e){
+            System.out.println("Falha na conexão com o Docker Mysql. Detalhes do erro: " + e.getMessage());
+            return false;
+        }catch (RuntimeException e){
+            System.out.println("Falha no carregamento do drive. Detalhes: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public void salvarAdotante(Adotante adotante) throws Exception {
+
+    }
+
+    @Override
+    public void salvarAnimal(Animal animal) throws Exception {
+
+    }
+
+    @Override
+    public void atualizarStatusAnimal(Animal animal) throws Exception {
+
+    }
+
+    @Override
+    public void salvarAdocao(Adocao adocao) throws Exception {
+
+    }
+
+    @Override
+    public Adotante buscarAdotantePorId(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Animal buscarAnimalPorId(int id) throws Exception {
+        return null;
+    }
 }
